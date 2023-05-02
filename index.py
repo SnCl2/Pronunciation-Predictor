0eba029ed312eb6c2dc7eb7b40454075da918e59
SOUMYADIP NAG <69238652+SnCl2@users.noreply.github.com>
2023-05-02 17:19:52 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,27 +2,30 @@
 """
 Created on Wed Mar  1 15:38:33 2023
 
-@author: User
+@author: Soumyadip Nag
 """
 
-
+# Importing dependency
 import streamlit as st
-from  text_to_speech import t_to_s
+#from  text_to_speech import t_to_s
 from dict_ import show_def
 from predict import predict_pronunciation
 
 
 
 
-
+# This is the main program for the webpage
 def predict_page():
     st.title("Hi I am Soumyadip !!!")
+    # Input fild for entering text
     word = st.text_input('What is the word',"Pizza").lower()
-    mike = st.button("o<=")
+#   mike = st.button("o<=")
+    # Submit button
     ok = st.button("Submit")
 
-    if mike:
-        t_to_s(word)
+#     if mike:
+#         t_to_s(word)
+    # Code for action of the ok button
     if ok:
         text = predict_pronunciation(word)
         st.subheader(f"Pronunciation:{text}")
