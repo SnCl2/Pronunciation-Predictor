e4ebb118256d5b1394efd9c5496ebe24b8a785f5
SOUMYADIP NAG <69238652+SnCl2@users.noreply.github.com>
2023-05-02 17:06:44 +0530

    Update dict_.py

--- dict_.py
+++ dict_.py
@@ -2,12 +2,13 @@
 """
 Created on Thu Mar  2 09:18:35 2023
 
-@author: User
+@author: Soumyadip Nag
 """
+# Importing dependency
 import pandas as pd
+# This program return the meaning of a given word from the english Dictionary.csv
 dict_path="english Dictionary.csv"
 df=pd.read_csv(dict_path)
-
 def show_def(word):
     return df[df["word"]==word]["def"].tolist()
 
