67a980dc4f449b84a5f439207f808a1f45b2a28b
SOUMYADIP NAG <69238652+SnCl2@users.noreply.github.com>
2023-05-02 17:43:15 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Mar  2 13:19:26 2023
 
-@author: User
+@author: Soumyadip Nag
 """
 import numpy as np
 from tensorflow.keras.models import  Model
@@ -13,18 +13,21 @@ import pickle
 char_vocab_size = 37
 phone_vocab_size = 78
 max_len_op = 30
+# Loading the importent component of the model
 
+# Tokenizer for the word (word to char)
 with open('char_tokenizer.pickle', 'rb') as handle:
     char_tokenizer = pickle.load(handle)
+# Tokenizer for the Pronunciation (word to char)
 with open('phone_tokenizer.pickle', 'rb') as handle:
     phone_tokenizer = pickle.load(handle)
-# Encoder
+# Creating the components of Encoder
 char_input = Input(shape=(None,))
 x = Embedding(char_vocab_size, 256, mask_zero=True)(char_input)
 x=LSTM(128, return_sequences=True)(x)
 output_y, state_h, state_c = LSTM(256, return_state=True)(x)
 
-# Decoder
+# Creating the components of Decoder
 ph_input = Input(shape=(None,))
 embedding_layer = Embedding(phone_vocab_size, 256, mask_zero=True)
 x = embedding_layer(ph_input)
@@ -34,9 +37,9 @@ softmax_dense = Dense(phone_vocab_size, activation='softmax')
 output = softmax_dense(output_y)
 
 model = Model(inputs=[char_input, ph_input],outputs=output)
-#model.compile(loss='sparse_categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
 
-#load w8
+
+#load weights of the model
 model.load_weights("model.h5")
 # Encoder
 encoder = Model(char_input, [state_h, state_c])
@@ -49,23 +52,23 @@ x, decoder_output_h, decoder_output_c = decoder_lstm(x, initial_state=[decoder_i
 x = softmax_dense(x)
 decoder = Model([ph_input] + [decoder_input_h, decoder_input_c], 
                                 [x] + [decoder_output_h, decoder_output_c])
-
+# Main code for prediction
 def predict_pronunciation(ch_input):
+    # Lowering the input
     ch_input=ch_input.lower()
+    # Tokenizing the input
     input_seq = char_tokenizer.texts_to_sequences([ch_input])
-
+    # Axising the cell state and hidden state of the incoder
     next_h, next_c = encoder.predict(input_seq,verbose=0)
-    #print('encoder done')
-
+    # Creating the start cockne
     curr_token = np.zeros((1,1))
     curr_token[0][0] = phone_tokenizer.word_index['startseq']
 
     pred_sentence = ''
-
+    
+    # Decoder Loop
     for i in range(max_len_op):
-        # print('entering decoder')
         output, next_h, next_c = decoder.predict([curr_token] + [next_h, next_c],verbose=0)
-        #print(output)
         next_token = np.argmax(output[0, 0, :])
         next_word = phone_tokenizer.index_word[next_token]
         if next_word == 'endseq':
