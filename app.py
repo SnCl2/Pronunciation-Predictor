adc08a5ccdf35c76cb06c3932b354c6500865989
SOUMYADIP NAG <69238652+SnCl2@users.noreply.github.com>
2023-05-02 16:30:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 """
 Created on Wed Mar  1 15:39:31 2023
 
-@author: User
+@author: Sounyadip Nag
 """
-
+# Importing dependency
 import streamlit as st
+# Importing The main programe
 from  index import predict_page
 
 predict_page()
